1f73bc1c22d87ee7101f8f5726718c03269a37a0
Keneth Alderete <dzecoft@gmail.com>
2026-07-12 22:18:14 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-self.addEventListener('fetch', function(event) {});
+// Este código obliga al celular a destruir TODA la memoria vieja y conectarse a internet.
+self.addEventListener('install', function(e) {
+  self.skipWaiting(); 
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      for (let name of names) caches.delete(name);
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  // Lo dejamos vacío intencionalmente para que NO use memoria y descargue el logo nuevo.
+});
